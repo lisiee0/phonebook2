fb81cd47fbd01262772be2faeb795bda8e456b2b
Hojun_Yoo <lisiee0@naver.com>
2022-01-07 16:13:17 +0900

    01-07

--- src/com/javaex/controller/PhonebookController.java
+++ src/com/javaex/controller/PhonebookController.java
@@ -19,22 +19,40 @@ public class PhonebookController extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		System.out.println("PhonebookController");
-		
-		PhoneDao pDao= new PhoneDao();
-		List<PhoneVo> pList= pDao.getPersonList();
-		
-		System.out.println(pList);
-		
-		// html 과 list 섞어서 표현해야함
-		// servlet 으로는 표현이 복잡함 --> jsp 이용한다
-		
-		request.setAttribute("pl", pList);
+		String act= request.getParameter("action");
 		
 		
-		// *포워드*
-		RequestDispatcher rd= request.getRequestDispatcher("/WEB-INF/list.jsp");
-		rd.forward(request, response);
+		// System.out.println("PhonebookController");
+		
+		if("list".equals(act)) {
+			System.out.println("action=list");
+			PhoneDao pDao= new PhoneDao();
+			List<PhoneVo> pList= pDao.getPersonList();
+		
+			System.out.println(pList);
+		
+			// html 과 list 섞어서 표현해야함
+			// servlet 으로는 표현이 복잡함 --> jsp 이용한다
+		
+			request.setAttribute("pl", pList);
+		
+			// *포워드*
+			RequestDispatcher rd= request.getRequestDispatcher("/WEB-INF/list.jsp");
+			rd.forward(request, response);
+	
+		}
+		else if("writeForm".equals(act)) {
+			System.out.println("전화번호 등록폼");
+			
+			RequestDispatcher rd= request.getRequestDispatcher("/WEB-INF/writeForm.jsp");
+			rd.forward(request, response);
+		}
+		else if("enroll".equals(act)) {
+			
+		}
+		else {
+			System.out.println("error");
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
